e30bfd77096a66effa2dd7f406fd9dbed06da9a9
TrashPanda <fd90278@gmail.com>
2020-02-18 15:13:06 +0800

    0218

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
-//這是一個前端工具
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-gulp.task('concat', function(){
-    //src經由pipe跑到dest的地點去
-    gulp.src('js/*.js').pipe(gulp.dest('dest/js'))
+gulp.task('img' ,function () {
+  gulp.src(['dev/img/*.*'  , 'dev/img/**/*.*']).pipe(gulp.dest('dest/img'));
+});
+
+gulp.task('concatjs', function(){
+    gulp.src('./dev/js/*.js').pipe(gulp.dest('dest/js'));
 });
 
 //壓縮css
@@ -16,18 +21,36 @@ gulp.task('concatcss' ,function () {
 });
 
 //scss轉譯
-//watch sass / gulp sass二選一，都是轉譯scss的工具
 gulp.task('sass' , function() {
-    gulp.src('sass/*.scss')
+    gulp.src('dev/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('dest/css/'));
 })
 
-//監看所有轉譯、串聯css的動作
-//跟watch sass功能很像
-//退出監看功能[ctrl+c]，mac系統的話[ctrl+z]
-gulp.task('watch', function(){
-    gulp.watch('sass/*.scss', ['sass']);//監看淺層scss
-    gulp.watch('sass/*.scss', ['sass/**/*.scss']);//監看深層scss(後面一個資料夾)
-    gulp.watch('js/*.js', ['concat']);
-})
+
+//打包 ./gulp/index.html 裡的檔案加上include指令
+//傳出打包後的 html 到 ./dest
+gulp.task('fileinclude', function() {
+    gulp.src(['*.html'])
+      .pipe(fileinclude({
+        prefix: '@@', //深層include
+        basepath: '@file' //層include
+      }))
+      .pipe(gulp.dest('./dest'));
+  });
+
+
+
+//最後要執行的時候只要在終端機輸入 gulp
+gulp.task('default', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./dest",
+          index: "index.html"
+      }
+  });
+  gulp.watch(['dev/*.html' , 'dev/**/*.html'],  ['fileinclude']).on('change',reload);
+  gulp.watch(['dev/sass/*.scss' , 'dev/sass/**/*.scss'], ['sass']).on('change',reload);
+  gulp.watch(['dev/js/*.js' , 'dev/js/**/*.js'], ['concatjs']).on('change',reload);
+  gulp.watch(['dev/img/*.*' , 'dev/img/**/*.*'] ,['img'] ).on('change',reload);
+});
